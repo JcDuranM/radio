204146c074a560f9222c3cf21b5cf90209bb3fcb
JcDuranM <72593176+JcDuranM@users.noreply.github.com>
2024-05-24 03:53:59 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ const URL_STREAMING = 'https://stream.zeno.fm/k8yuexp35k0uv';
 const url = 'https://api.zeno.fm/mounts/metadata/subscribe/k8yuexp35k0uv';
 
 // Visit https://api.vagalume.com.br/docs/ to get your API key
-//const API_KEY = "18fe07917957c289983464588aabddfb";
+const API_KEY = "18fe07917957c289983464588aabddfb";
 
 // Variable to control history display: true = display / false = hides
 let showHistory = true; 
