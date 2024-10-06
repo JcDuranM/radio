afe79b11ecc7a66445a552c5db9cd9ae512b678e
JcDuranM <72593176+JcDuranM@users.noreply.github.com>
2024-10-06 16:06:48 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,11 @@
-// Define o nome do cache
-const CACHE_NAME = 'web-radio-v1';
+self.addEventListener('push', function(event) {
+    const options = {
+        body: event.data ? event.data.text() : 'Notificación sin cuerpo',
+        icon: 'icon.png', // Ruta al icono de la notificación
+        badge: 'badge.png' // Ruta a la imagen de la insignia
+    };
 
-// Lista de arquivos a serem cacheados
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/script.js',
-  '/img/cover.png',
-  // Adicione outros recursos que deseja cache aqui
-];
-
-// Instala o Service Worker e adiciona os arquivos ao cache
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Cache aberto');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Intercepta as solicitações e serve os arquivos em cache se disponíveis
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - retorna a resposta do cache
-        if (response) {
-          return response;
-        }
-        // Não encontrado no cache - busca na rede
-        return fetch(event.request);
-      }
-    )
-  );
+    event.waitUntil(
+        self.registration.showNotification('Renta de Carritos Montables', options)
+    );
 });
